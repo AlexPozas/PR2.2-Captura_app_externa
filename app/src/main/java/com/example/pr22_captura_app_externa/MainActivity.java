511308a8fd5ff586fabbb915615ca9d24f1a726a
AlexPozas <97431370+AlexPozas@users.noreply.github.com>
2023-12-05 17:46:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/pr22_captura_app_externa/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pr22_captura_app_externa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
